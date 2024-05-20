64409a8fc301a57f9390c95511ca2c371b95561a
abdul-aleemelmozogi <abddo.55242@gmail.com>
2024-05-20 10:23:19 +0200

    adds

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,11 @@
+// ********************************************************************************
+// * This project structure was built by Abdul-Aleem El-Mozughi.                  *
+// * I hope you find the template useful!                                         *
+// *                                                                              *
+// * GitHub: https://github.com/aleemElmozogi                                     *
+// *                                                                              *
+// ********************************************************************************
+
 import 'package:bloc/bloc.dart';
 import 'package:pretty_bloc_observer/pretty_bloc_observer.dart';
 import 'core/di/injection.dart' as di;
@@ -5,6 +13,7 @@ import 'package:flutter/material.dart';
 
 import 'app.dart';
 import 'core/enums/environment_types.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
