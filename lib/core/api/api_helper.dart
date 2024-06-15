d35bd08211d4aa4df8a53ea7b91403a77f830436
Abdulalim Elmozogi <53140500+aleemElmozogi@users.noreply.github.com>
2024-06-15 12:55:02 +0200

    adding _handleBackEndError

--- lib/core/api/api_helper.dart
+++ lib/core/api/api_helper.dart
@@ -41,8 +41,16 @@ class ApiHelperImpl implements ApiHelper {
     }
   }
 
+
+
   @override
   dynamic handleDioError(DioException error) {
+    try {
+      _handleBackEndError(error.response);
+    } catch (e) {
+      // Rethrow the exception to prevent execution of the switch statement
+      rethrow;
+    }
     switch (error.type) {
       case DioExceptionType.connectionTimeout:
       case DioExceptionType.sendTimeout:
@@ -73,18 +81,34 @@ class ApiHelperImpl implements ApiHelper {
       final messageResponse = jsonDecode(response.data!);
 
       if (response.statusCode != null &&
-              response.statusCode! >= 200 &&
-              response.statusCode! < 300) {
+          response.statusCode! >= 200 &&
+          response.statusCode! < 300) {
         // Handle success response
         parsedResponse = parsedResponse.fromJson(messageResponse);
         return parsedResponse as T;
       } else {
         // Handle error response
-        final errorResponse = ErrorResponseModel.fromJson(messageResponse);
-        throw ApiException(errorResponse);
+        _handleBackEndError(response);
+        // Since _handleBackEndError will throw an exception, the next line is unreachable.
+        // However, Dart requires a return statement to satisfy the return type.
+        throw FetchDataException(); // or any default error handling
       }
     } catch (e) {
       throw BadResponseException(e.toString());
+    }
+  }
+
+
+  dynamic _handleBackEndError(Response<dynamic>? response) {
+    if (response != null) {
+      try{
+        final decodedResponse = jsonDecode(response.data!);
+        final errorResponse = ErrorResponseModel.fromJson(decodedResponse);
+        throw ApiException(errorResponse);
+      }on Exception catch(e){
+        throw FetchDataException();
+      }
+
     }
   }
 }
