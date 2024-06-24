8d6e806fadc77de999b5328b48c797e6091a7860
Abdulalim Elmozogi <53140500+aleemElmozogi@users.noreply.github.com>
2024-06-24 17:13:47 +0200

    i10 generated messages

---
+++ lib/generated/intl/messages_ar.dart
@@ -0,0 +1,50 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ar locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ar';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "cancel": MessageLookupByLibrary.simpleMessage("إلغاء"),
+        "check": MessageLookupByLibrary.simpleMessage("تحقق"),
+        "conflictOccurred": MessageLookupByLibrary.simpleMessage("حدث تضارب"),
+        "errorDuringCommunication":
+            MessageLookupByLibrary.simpleMessage("خطأ أثناء التواصل"),
+        "helloWorld": MessageLookupByLibrary.simpleMessage("مرحبا بالعالم!"),
+        "informationNotAvailable": MessageLookupByLibrary.simpleMessage(
+            "المعلومات المطلوبة غير موجودة"),
+        "internalServerError":
+            MessageLookupByLibrary.simpleMessage("خطأ داخلي في الخادم"),
+        "invalidRequest": MessageLookupByLibrary.simpleMessage("طلب غير صالح"),
+        "invalidResponse":
+            MessageLookupByLibrary.simpleMessage("استجابة غير صالحة"),
+        "logout": MessageLookupByLibrary.simpleMessage("تسجيل خروج"),
+        "noDataOrContentAvailable": MessageLookupByLibrary.simpleMessage(
+            "لا يوجد بيانات أو محتوى متاح"),
+        "noInternetConnection":
+            MessageLookupByLibrary.simpleMessage("لا يوجد اتصال بالإنترنت"),
+        "ok": MessageLookupByLibrary.simpleMessage("موافق"),
+        "opps": MessageLookupByLibrary.simpleMessage("عفواً !"),
+        "resend": MessageLookupByLibrary.simpleMessage("أعد الإرسال"),
+        "save": MessageLookupByLibrary.simpleMessage("حفظ"),
+        "unauthorized": MessageLookupByLibrary.simpleMessage("غير مصرح به"),
+        "welcome": MessageLookupByLibrary.simpleMessage("أهلًا بك")
+      };
+}
---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,86 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "add_friend": MessageLookupByLibrary.simpleMessage("Add Friend"),
+        "app_name": MessageLookupByLibrary.simpleMessage("My Flutter Template"),
+        "cancel": MessageLookupByLibrary.simpleMessage("Cancel"),
+        "change_email": MessageLookupByLibrary.simpleMessage("Change Email"),
+        "change_password":
+            MessageLookupByLibrary.simpleMessage("Change Password"),
+        "check": MessageLookupByLibrary.simpleMessage("Check"),
+        "code_has_been_sent": MessageLookupByLibrary.simpleMessage(
+            "The code has been sent to your phone number"),
+        "code_will_be_sent": MessageLookupByLibrary.simpleMessage(
+            "A code will be sent to the phone number"),
+        "conflictOccurred":
+            MessageLookupByLibrary.simpleMessage("A conflict occurred"),
+        "continio": MessageLookupByLibrary.simpleMessage("Continue"),
+        "enter_new_pin": MessageLookupByLibrary.simpleMessage("Enter new PIN"),
+        "enter_number_to_add": MessageLookupByLibrary.simpleMessage(
+            "Enter the number of the friend to be added"),
+        "enter_pin": MessageLookupByLibrary.simpleMessage("Enter PIN"),
+        "errorDuringCommunication":
+            MessageLookupByLibrary.simpleMessage("Error during communication"),
+        "helloWorld": MessageLookupByLibrary.simpleMessage("Hello World!"),
+        "informationNotAvailable": MessageLookupByLibrary.simpleMessage(
+            "The requested information is not available"),
+        "internalServerError":
+            MessageLookupByLibrary.simpleMessage("Internal server error"),
+        "internet_connection_error": MessageLookupByLibrary.simpleMessage(
+            "Please check your internet connection and try again"),
+        "invalidRequest":
+            MessageLookupByLibrary.simpleMessage("Invalid request"),
+        "invalidResponse":
+            MessageLookupByLibrary.simpleMessage("Invalid response"),
+        "logout": MessageLookupByLibrary.simpleMessage("Log Out"),
+        "noDataOrContentAvailable": MessageLookupByLibrary.simpleMessage(
+            "No data or content available"),
+        "noInternetConnection":
+            MessageLookupByLibrary.simpleMessage("No internet connection"),
+        "ok": MessageLookupByLibrary.simpleMessage("OK"),
+        "opps": MessageLookupByLibrary.simpleMessage("Oops!"),
+        "password_error_message": MessageLookupByLibrary.simpleMessage(
+            "Password must be at least 8 digits"),
+        "phone_error_message": MessageLookupByLibrary.simpleMessage(
+            "Please enter the phone number in the correct format"),
+        "qr_parsing_error": MessageLookupByLibrary.simpleMessage(
+            "An error occurred while parsing the QR code"),
+        "reload_screen": MessageLookupByLibrary.simpleMessage("Reload Screen"),
+        "resend": MessageLookupByLibrary.simpleMessage("Resend"),
+        "save": MessageLookupByLibrary.simpleMessage("Save"),
+        "singin_with_phone":
+            MessageLookupByLibrary.simpleMessage("Sign in with phone number"),
+        "something_went_wrong":
+            MessageLookupByLibrary.simpleMessage("Something went wrong"),
+        "try_again": MessageLookupByLibrary.simpleMessage("Try Again"),
+        "unauthorized": MessageLookupByLibrary.simpleMessage("Unauthorized"),
+        "use_strong_pin": MessageLookupByLibrary.simpleMessage(
+            "Use a strong PIN for your account"),
+        "verify_with_phone":
+            MessageLookupByLibrary.simpleMessage("Verify with phone number"),
+        "want_to_logout":
+            MessageLookupByLibrary.simpleMessage("Do you want to log out?"),
+        "welcome": MessageLookupByLibrary.simpleMessage("Welcome")
+      };
+}
