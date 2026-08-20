a46673260aadbb4758b20bd2c17a7d3279c93599
abdul-aleem <a.elmozogi@masarat.ly>
2026-08-20 13:19:29 +0200

    Update Android build configuration and dependencies
    This commit updates the Android build environment by:
    - Upgrading the Android Gradle Plugin (AGP) version from `7.3.0` to `8.11.1`.
    - Upgrading the Kotlin Android plugin version from `1.7.10` to `2.2.20`.
    - Updating the Gradle wrapper distribution from version `7.6.3` to `8.14`.

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -22,67 +22,85 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "addFriend": MessageLookupByLibrary.simpleMessage("إضافة صديق"),
-        "agree": MessageLookupByLibrary.simpleMessage("حسنًا"),
-        "appName": MessageLookupByLibrary.simpleMessage("تمبلت فلاتر"),
-        "cancel": MessageLookupByLibrary.simpleMessage("إلغاء"),
-        "changeEmail":
-            MessageLookupByLibrary.simpleMessage("تغيير البريد الإلكتروني"),
-        "changePassword":
-            MessageLookupByLibrary.simpleMessage("تغيير كلمة المرور"),
-        "check": MessageLookupByLibrary.simpleMessage("تحقق"),
-        "codeHasBeenSent": MessageLookupByLibrary.simpleMessage(
-            "تم إرسال الرمز إلى رقم هاتفك"),
-        "codeWillBeSent": MessageLookupByLibrary.simpleMessage(
-            "سيتم إرسال رمز إلى رقم الهاتف"),
-        "conflictOccurred": MessageLookupByLibrary.simpleMessage("حدث تضارب"),
-        "enterNewPin": MessageLookupByLibrary.simpleMessage(
-            "أدخل رمز التعريف الشخصي الجديد"),
-        "enterNumberToAdd": MessageLookupByLibrary.simpleMessage(
-            "أدخل رقم الصديق المراد إضافته"),
-        "enterPin":
-            MessageLookupByLibrary.simpleMessage("أدخل رمز التعريف الشخصي"),
-        "errorDuringCommunication":
-            MessageLookupByLibrary.simpleMessage("خطأ أثناء التواصل"),
-        "helloWorld": MessageLookupByLibrary.simpleMessage("مرحبا بالعالم!"),
-        "informationNotAvailable": MessageLookupByLibrary.simpleMessage(
-            "المعلومات المطلوبة غير موجودة"),
-        "internalServerError":
-            MessageLookupByLibrary.simpleMessage("خطأ داخلي في الخادم"),
-        "internetConnectionError": MessageLookupByLibrary.simpleMessage(
-            "من فضلك أفحص اتصالك بالانترنت ، وأعد المحاولة"),
-        "invalidRequest": MessageLookupByLibrary.simpleMessage("طلب غير صالح"),
-        "invalidResponse":
-            MessageLookupByLibrary.simpleMessage("استجابة غير صالحة"),
-        "logout": MessageLookupByLibrary.simpleMessage("تسجيل خروج"),
-        "noDataOrContentAvailable": MessageLookupByLibrary.simpleMessage(
-            "لا يوجد بيانات أو محتوى متاح"),
-        "noInternetConnection":
-            MessageLookupByLibrary.simpleMessage("لا يوجد اتصال بالإنترنت"),
-        "ok": MessageLookupByLibrary.simpleMessage("موافق"),
-        "opps": MessageLookupByLibrary.simpleMessage("عفواً !"),
-        "passwordErrorMessage": MessageLookupByLibrary.simpleMessage(
-            "يجب ان تتكون كلمة المرور من 8 أرقام على الأقل"),
-        "phoneErrorMessage": MessageLookupByLibrary.simpleMessage(
-            "الرجاء إدخال رقم الهاتف بالصيغة الصحيحة"),
-        "qrParsingError": MessageLookupByLibrary.simpleMessage(
-            "حدث خطأ أثناء تجهيز رمز الQR"),
-        "reloadScreen":
-            MessageLookupByLibrary.simpleMessage("إعادة تحميل الصفحة"),
-        "resend": MessageLookupByLibrary.simpleMessage("أعد الإرسال"),
-        "save": MessageLookupByLibrary.simpleMessage("حفظ"),
-        "signinWithPhone":
-            MessageLookupByLibrary.simpleMessage("تسجيل الدخول برقم الهاتف"),
-        "somethingWentWrong":
-            MessageLookupByLibrary.simpleMessage("حدث خطأ ما"),
-        "tryAgain": MessageLookupByLibrary.simpleMessage("حاول مجدداَ"),
-        "unauthorized": MessageLookupByLibrary.simpleMessage("غير مصرح به"),
-        "useStrongPin": MessageLookupByLibrary.simpleMessage(
-            "إستخدم رمز تعريف شخصي قوي لحسابك"),
-        "verifyWithPhone":
-            MessageLookupByLibrary.simpleMessage("التحقق من رقم الهاتف"),
-        "wantToLogout":
-            MessageLookupByLibrary.simpleMessage("هل تريد تسجيل الخروج؟"),
-        "welcome": MessageLookupByLibrary.simpleMessage("أهلًا بك")
-      };
+    "addFriend": MessageLookupByLibrary.simpleMessage("إضافة صديق"),
+    "agree": MessageLookupByLibrary.simpleMessage("حسنًا"),
+    "appName": MessageLookupByLibrary.simpleMessage("تمبلت فلاتر"),
+    "cancel": MessageLookupByLibrary.simpleMessage("إلغاء"),
+    "changeEmail": MessageLookupByLibrary.simpleMessage(
+      "تغيير البريد الإلكتروني",
+    ),
+    "changePassword": MessageLookupByLibrary.simpleMessage("تغيير كلمة المرور"),
+    "check": MessageLookupByLibrary.simpleMessage("تحقق"),
+    "codeHasBeenSent": MessageLookupByLibrary.simpleMessage(
+      "تم إرسال الرمز إلى رقم هاتفك",
+    ),
+    "codeWillBeSent": MessageLookupByLibrary.simpleMessage(
+      "سيتم إرسال رمز إلى رقم الهاتف",
+    ),
+    "conflictOccurred": MessageLookupByLibrary.simpleMessage("حدث تضارب"),
+    "enterNewPin": MessageLookupByLibrary.simpleMessage(
+      "أدخل رمز التعريف الشخصي الجديد",
+    ),
+    "enterNumberToAdd": MessageLookupByLibrary.simpleMessage(
+      "أدخل رقم الصديق المراد إضافته",
+    ),
+    "enterPin": MessageLookupByLibrary.simpleMessage("أدخل رمز التعريف الشخصي"),
+    "errorDuringCommunication": MessageLookupByLibrary.simpleMessage(
+      "خطأ أثناء التواصل",
+    ),
+    "helloWorld": MessageLookupByLibrary.simpleMessage("مرحبا بالعالم!"),
+    "informationNotAvailable": MessageLookupByLibrary.simpleMessage(
+      "المعلومات المطلوبة غير موجودة",
+    ),
+    "internalServerError": MessageLookupByLibrary.simpleMessage(
+      "خطأ داخلي في الخادم",
+    ),
+    "internetConnectionError": MessageLookupByLibrary.simpleMessage(
+      "من فضلك أفحص اتصالك بالانترنت ، وأعد المحاولة",
+    ),
+    "invalidRequest": MessageLookupByLibrary.simpleMessage("طلب غير صالح"),
+    "invalidResponse": MessageLookupByLibrary.simpleMessage(
+      "استجابة غير صالحة",
+    ),
+    "logout": MessageLookupByLibrary.simpleMessage("تسجيل خروج"),
+    "noDataOrContentAvailable": MessageLookupByLibrary.simpleMessage(
+      "لا يوجد بيانات أو محتوى متاح",
+    ),
+    "noInternetConnection": MessageLookupByLibrary.simpleMessage(
+      "لا يوجد اتصال بالإنترنت",
+    ),
+    "ok": MessageLookupByLibrary.simpleMessage("موافق"),
+    "opps": MessageLookupByLibrary.simpleMessage("عفواً !"),
+    "otpErrorMessage": MessageLookupByLibrary.simpleMessage(
+      "الرجاء التأكد من كتابة رمز الـOTP بشكل صحيح",
+    ),
+    "passwordErrorMessage": MessageLookupByLibrary.simpleMessage(
+      "يجب ان تتكون كلمة المرور من 8 أرقام على الأقل",
+    ),
+    "phoneErrorMessage": MessageLookupByLibrary.simpleMessage(
+      "الرجاء إدخال رقم الهاتف بالصيغة الصحيحة",
+    ),
+    "qrParsingError": MessageLookupByLibrary.simpleMessage(
+      "حدث خطأ أثناء تجهيز رمز الQR",
+    ),
+    "reloadScreen": MessageLookupByLibrary.simpleMessage("إعادة تحميل الصفحة"),
+    "resend": MessageLookupByLibrary.simpleMessage("أعد الإرسال"),
+    "save": MessageLookupByLibrary.simpleMessage("حفظ"),
+    "signinWithPhone": MessageLookupByLibrary.simpleMessage(
+      "تسجيل الدخول برقم الهاتف",
+    ),
+    "somethingWentWrong": MessageLookupByLibrary.simpleMessage("حدث خطأ ما"),
+    "tryAgain": MessageLookupByLibrary.simpleMessage("حاول مجدداَ"),
+    "unauthorized": MessageLookupByLibrary.simpleMessage("غير مصرح به"),
+    "useStrongPin": MessageLookupByLibrary.simpleMessage(
+      "إستخدم رمز تعريف شخصي قوي لحسابك",
+    ),
+    "verifyWithPhone": MessageLookupByLibrary.simpleMessage(
+      "التحقق من رقم الهاتف",
+    ),
+    "wantToLogout": MessageLookupByLibrary.simpleMessage(
+      "هل تريد تسجيل الخروج؟",
+    ),
+    "welcome": MessageLookupByLibrary.simpleMessage("أهلًا بك"),
+  };
 }
--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -22,66 +22,84 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "addFriend": MessageLookupByLibrary.simpleMessage("Add Friend"),
-        "agree": MessageLookupByLibrary.simpleMessage("Agree"),
-        "appName": MessageLookupByLibrary.simpleMessage("My Flutter Template"),
-        "cancel": MessageLookupByLibrary.simpleMessage("Cancel"),
-        "changeEmail": MessageLookupByLibrary.simpleMessage("Change Email"),
-        "changePassword":
-            MessageLookupByLibrary.simpleMessage("Change Password"),
-        "check": MessageLookupByLibrary.simpleMessage("Check"),
-        "codeHasBeenSent": MessageLookupByLibrary.simpleMessage(
-            "The code has been sent to your phone number"),
-        "codeWillBeSent": MessageLookupByLibrary.simpleMessage(
-            "A code will be sent to the phone number"),
-        "conflictOccurred":
-            MessageLookupByLibrary.simpleMessage("A conflict occurred"),
-        "continio": MessageLookupByLibrary.simpleMessage("Continue"),
-        "enterNewPin": MessageLookupByLibrary.simpleMessage("Enter new PIN"),
-        "enterNumberToAdd": MessageLookupByLibrary.simpleMessage(
-            "Enter the number of the friend to be added"),
-        "enterPin": MessageLookupByLibrary.simpleMessage("Enter PIN"),
-        "errorDuringCommunication":
-            MessageLookupByLibrary.simpleMessage("Error during communication"),
-        "helloWorld": MessageLookupByLibrary.simpleMessage("Hello World!"),
-        "informationNotAvailable": MessageLookupByLibrary.simpleMessage(
-            "The requested information is not available"),
-        "internalServerError":
-            MessageLookupByLibrary.simpleMessage("Internal server error"),
-        "internetConnectionError": MessageLookupByLibrary.simpleMessage(
-            "Please check your internet connection and try again"),
-        "invalidRequest":
-            MessageLookupByLibrary.simpleMessage("Invalid request"),
-        "invalidResponse":
-            MessageLookupByLibrary.simpleMessage("Invalid response"),
-        "logout": MessageLookupByLibrary.simpleMessage("Log Out"),
-        "noDataOrContentAvailable": MessageLookupByLibrary.simpleMessage(
-            "No data or content available"),
-        "noInternetConnection":
-            MessageLookupByLibrary.simpleMessage("No internet connection"),
-        "ok": MessageLookupByLibrary.simpleMessage("OK"),
-        "opps": MessageLookupByLibrary.simpleMessage("Oops!"),
-        "passwordErrorMessage": MessageLookupByLibrary.simpleMessage(
-            "Password must be at least 8 digits"),
-        "phoneErrorMessage": MessageLookupByLibrary.simpleMessage(
-            "Please enter the phone number in the correct format"),
-        "qrParsingError": MessageLookupByLibrary.simpleMessage(
-            "An error occurred while parsing the QR code"),
-        "reloadScreen": MessageLookupByLibrary.simpleMessage("Reload Screen"),
-        "resend": MessageLookupByLibrary.simpleMessage("Resend"),
-        "save": MessageLookupByLibrary.simpleMessage("Save"),
-        "signinWithPhone":
-            MessageLookupByLibrary.simpleMessage("Sign in with phone number"),
-        "somethingWentWrong":
-            MessageLookupByLibrary.simpleMessage("Something went wrong"),
-        "tryAgain": MessageLookupByLibrary.simpleMessage("Try Again"),
-        "unauthorized": MessageLookupByLibrary.simpleMessage("Unauthorized"),
-        "useStrongPin": MessageLookupByLibrary.simpleMessage(
-            "Use a strong PIN for your account"),
-        "verifyWithPhone":
-            MessageLookupByLibrary.simpleMessage("Verify with phone number"),
-        "wantToLogout":
-            MessageLookupByLibrary.simpleMessage("Do you want to log out?"),
-        "welcome": MessageLookupByLibrary.simpleMessage("Welcome")
-      };
+    "addFriend": MessageLookupByLibrary.simpleMessage("Add Friend"),
+    "agree": MessageLookupByLibrary.simpleMessage("Agree"),
+    "appName": MessageLookupByLibrary.simpleMessage("My Flutter Template"),
+    "cancel": MessageLookupByLibrary.simpleMessage("Cancel"),
+    "changeEmail": MessageLookupByLibrary.simpleMessage("Change Email"),
+    "changePassword": MessageLookupByLibrary.simpleMessage("Change Password"),
+    "check": MessageLookupByLibrary.simpleMessage("Check"),
+    "codeHasBeenSent": MessageLookupByLibrary.simpleMessage(
+      "The code has been sent to your phone number",
+    ),
+    "codeWillBeSent": MessageLookupByLibrary.simpleMessage(
+      "A code will be sent to the phone number",
+    ),
+    "conflictOccurred": MessageLookupByLibrary.simpleMessage(
+      "A conflict occurred",
+    ),
+    "continio": MessageLookupByLibrary.simpleMessage("Continue"),
+    "enterNewPin": MessageLookupByLibrary.simpleMessage("Enter new PIN"),
+    "enterNumberToAdd": MessageLookupByLibrary.simpleMessage(
+      "Enter the number of the friend to be added",
+    ),
+    "enterPin": MessageLookupByLibrary.simpleMessage("Enter PIN"),
+    "errorDuringCommunication": MessageLookupByLibrary.simpleMessage(
+      "Error during communication",
+    ),
+    "helloWorld": MessageLookupByLibrary.simpleMessage("Hello World!"),
+    "informationNotAvailable": MessageLookupByLibrary.simpleMessage(
+      "The requested information is not available",
+    ),
+    "internalServerError": MessageLookupByLibrary.simpleMessage(
+      "Internal server error",
+    ),
+    "internetConnectionError": MessageLookupByLibrary.simpleMessage(
+      "Please check your internet connection and try again",
+    ),
+    "invalidRequest": MessageLookupByLibrary.simpleMessage("Invalid request"),
+    "invalidResponse": MessageLookupByLibrary.simpleMessage("Invalid response"),
+    "logout": MessageLookupByLibrary.simpleMessage("Log Out"),
+    "noDataOrContentAvailable": MessageLookupByLibrary.simpleMessage(
+      "No data or content available",
+    ),
+    "noInternetConnection": MessageLookupByLibrary.simpleMessage(
+      "No internet connection",
+    ),
+    "ok": MessageLookupByLibrary.simpleMessage("OK"),
+    "opps": MessageLookupByLibrary.simpleMessage("Oops!"),
+    "otpErrorMessage": MessageLookupByLibrary.simpleMessage(
+      "Please enter the otp in correct form",
+    ),
+    "passwordErrorMessage": MessageLookupByLibrary.simpleMessage(
+      "Password must be at least 8 digits",
+    ),
+    "phoneErrorMessage": MessageLookupByLibrary.simpleMessage(
+      "Please enter the phone number in the correct format",
+    ),
+    "qrParsingError": MessageLookupByLibrary.simpleMessage(
+      "An error occurred while parsing the QR code",
+    ),
+    "reloadScreen": MessageLookupByLibrary.simpleMessage("Reload Screen"),
+    "resend": MessageLookupByLibrary.simpleMessage("Resend"),
+    "save": MessageLookupByLibrary.simpleMessage("Save"),
+    "signinWithPhone": MessageLookupByLibrary.simpleMessage(
+      "Sign in with phone number",
+    ),
+    "somethingWentWrong": MessageLookupByLibrary.simpleMessage(
+      "Something went wrong",
+    ),
+    "tryAgain": MessageLookupByLibrary.simpleMessage("Try Again"),
+    "unauthorized": MessageLookupByLibrary.simpleMessage("Unauthorized"),
+    "useStrongPin": MessageLookupByLibrary.simpleMessage(
+      "Use a strong PIN for your account",
+    ),
+    "verifyWithPhone": MessageLookupByLibrary.simpleMessage(
+      "Verify with phone number",
+    ),
+    "wantToLogout": MessageLookupByLibrary.simpleMessage(
+      "Do you want to log out?",
+    ),
+    "welcome": MessageLookupByLibrary.simpleMessage("Welcome"),
+  };
 }
